3fe3e24cb2bbd879e573318091023591bd930c81
Daniel Benjamin Brielbeck <crispybrain82@gmail.com>
2013-06-12 14:24:48 +0200

    SessionManager implemented

---
+++ domination/src/main/java/edu/hm/webtech/domination/manager/lobby/LobbyManager.java
@@ -0,0 +1,41 @@
+package edu.hm.webtech.domination.manager.lobby;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import edu.hm.webtech.domination.exception.ModelException;
+import edu.hm.webtech.domination.manager.game.IGameManager;
+import edu.hm.webtech.domination.model.IGameConfiguration;
+import edu.hm.webtech.domination.model.Player;
+
+public class LobbyManager implements ILobbyManager {
+
+	private List<Player> waitingPlayers;
+
+	public LobbyManager() {
+		this.waitingPlayers = new ArrayList<Player>();
+	}
+
+	@Override
+	public IGameManager createGame(IGameConfiguration gameConfiguration) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<IGameManager> getGames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void addWaitingPlayer(Player player) throws ModelException {
+		for (Player p : waitingPlayers) {
+			if(p.getIdentifier().equals(player.getIdentifier())) throw new ModelException("Player with username "+player.getIdentifier()+" already exists!");
+		}
+		waitingPlayers.add(player);
+	}
+
+}
---
+++ domination/src/main/java/edu/hm/webtech/domination/manager/session/SessionManager.java
@@ -0,0 +1,55 @@
+package edu.hm.webtech.domination.manager.session;
+
+import com.vaadin.addon.touchkit.ui.NavigationView;
+import com.vaadin.ui.LoginForm.LoginEvent;
+import com.vaadin.ui.LoginForm.LoginListener;
+
+import edu.hm.webtech.domination.MyVaadinApplication;
+import edu.hm.webtech.domination.exception.ModelException;
+import edu.hm.webtech.domination.manager.lobby.ILobbyManager;
+import edu.hm.webtech.domination.manager.lobby.LobbyManager;
+import edu.hm.webtech.domination.model.Player;
+
+/**
+ * This class adds users, if the don't already exist, to a list of waiting players in the lobby.
+ * 
+ * @author Daniel Brielbeck
+ * 
+ */
+public class SessionManager implements ISessionManager, LoginListener {
+
+	private LobbyManager lobbyManager;
+
+	 /** Ctor, intializing this with the given lobbymanager.
+	  *  Lobby manager is needed to add Players to the lobby.
+	  * 
+	  * @param lm
+	  *            LobbyManager of the App
+	  */
+	public SessionManager(ILobbyManager lm){
+		this.lobbyManager = (LobbyManager) lm;
+	}
+	
+	@Override
+	public void createAndRegisterPlayer(String identifier) {
+		try {
+			Player player = new Player(0, 0, identifier);
+			lobbyManager.addWaitingPlayer(player);
+			MyVaadinApplication.getApp().getMainWindow().getApplication().setUser(player);
+			// TODO Create and open Lobby View 
+		} catch (ModelException me) {
+			System.out.println(me.getMessage());
+		}		
+	}
+
+	/**
+	 * Called by click on login. Delegates chosen username to method createAndRegisterPlayer
+	 * @param event
+	 * 		holds the login data from the LoginEvent Object 
+	 */
+	@Override
+	public void onLogin(LoginEvent event) {
+		createAndRegisterPlayer(event.getLoginParameter("username"));
+	}
+
+}
