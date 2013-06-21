5eb626207de43405f134e424aa9598256b52d4dc
BastusIII <sebastian.stumpf87@googlemail.com>
2013-06-21 11:13:02 +0200

    fixed createCustomGame

--- domination/src/main/java/edu/hm/webtech/test/GameFactory.java
+++ domination/src/main/java/edu/hm/webtech/test/GameFactory.java
@@ -3,6 +3,8 @@ package edu.hm.webtech.test;
 import edu.hm.webtech.domination.exception.ModelException;
 import edu.hm.webtech.domination.model.*;
 
+import java.util.ArrayList;
+
 /**
  * A Factory creating dummy games for testing reasons.
  * @author Maximilian Briglmeier
@@ -19,26 +21,39 @@ public class GameFactory {
 
         IGame game = null;
 
+        ArrayList<ITeam> teams = new ArrayList<>();
+        teams.add(new Team(TeamIdentifier.BLUE, 0));
+        teams.add(new Team(TeamIdentifier.RED, 0));
+
+        // Initialize HMBACKYARD fields
+        IDominationPoint[] dominationPointsHMBackYard = new IDominationPoint[3];
+        dominationPointsHMBackYard[0] = new DominationPoint(11.555418, 48.154221, 10, 50);
+        dominationPointsHMBackYard[1] = new DominationPoint(11.556129, 48.154171, 10, 100);
+        dominationPointsHMBackYard[2] = new DominationPoint(11.556526, 48.153536, 10, 100);
+        Map mapHMBackYard = new Map(11.556062, 48.153991,18);
 		
         switch(gameConfiguration.getGameType()) {
             case HM_BACKYARD :
             	game = new Game(gameConfiguration);
-            	// Location for the HM Garden
-            	IMap map = new Map(11.556062, 48.153991,18);
-            	game.setMap(map);
+            	game.setMap(mapHMBackYard);
+                try {
+                    for (IDominationPoint domPoint : dominationPointsHMBackYard)
+                        game.addDominationPoint(domPoint);
+                    for(ITeam team : teams) {
+                        game.addTeam(team);
+                    }
+                } catch (ModelException ex) {
+                    ex.printStackTrace();
+                }
                 break;
             /**
              * Serves as dummy and exists for testing reasons. Represents a domination points 
              * and simulated movement of players in the HM garden.
              */
             case HM_BACKYARD_DUMMY:
-                IDominationPoint[] dominationPoints = new IDominationPoint[3];
-                dominationPoints[0] = new DominationPoint(11.555418, 48.154221, 10, 50);
-                dominationPoints[1] = new DominationPoint(11.556129, 48.154171, 10, 100);
-                dominationPoints[2] = new DominationPoint(11.556526, 48.153536, 10, 100);
 
-                ITeam teamBlue = new Team(TeamIdentifier.BLUE, 0);
-                ITeam teamRed = new Team(TeamIdentifier.RED, 0);
+                ITeam teamBlue = teams.get(0);
+                ITeam teamRed = teams.get(1);
 
                 final IPlayer[] players = new IPlayer[6];
                 players[0] = new Player(11.556062, 48.153991, "Player One", teamRed);
@@ -48,19 +63,20 @@ public class GameFactory {
                 players[4] = new Player(11.556300, 48.153790, "Player Five", teamBlue);
                 players[5] = new Player(11.556711, 48.153617, "Player Six", teamBlue);
 
-                game = new Game(new GameConfiguration(250,18,2,players.length / 2,dominationPoints.length,players[0],null,gameConfiguration.getName()));
+                game = new Game(new GameConfiguration(250,18,2,players.length / 2,dominationPointsHMBackYard.length,gameConfiguration.getOwner(),null,gameConfiguration.getName()));
                 game.setMap(new Map(11.556062, 48.153991, 18));
-                
-                dominationPoints[0].setOwnerTeam(teamRed);
-                dominationPoints[2].setOwnerTeam(teamBlue);
+
+                dominationPointsHMBackYard[0].setOwnerTeam(teamRed);
+                dominationPointsHMBackYard[2].setOwnerTeam(teamBlue);
 
                 try {
                     for (IPlayer player : players)
                         game.addPlayer(player);
-                    for (IDominationPoint domPoint : dominationPoints)
+                    for (IDominationPoint domPoint : dominationPointsHMBackYard)
                         game.addDominationPoint(domPoint);
-                    game.addTeam(teamRed);
-                    game.addTeam(teamBlue);
+                    for(ITeam team : teams) {
+                        game.addTeam(team);
+                    }
                 } catch (ModelException ex) {
                     ex.printStackTrace();
                 }
